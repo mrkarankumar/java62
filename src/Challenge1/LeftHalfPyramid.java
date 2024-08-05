8f8bfc8ee8c8514d89750c22adeca00c8adc55f8
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-08-05 15:46:08 +0530

    commit

---
+++ src/Challenge1/LeftHalfPyramid.java
@@ -0,0 +1,13 @@
+package Challenge1;
+
+public class LeftHalfPyramid {
+
+    public static void main(String[] args) {
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+
+}
